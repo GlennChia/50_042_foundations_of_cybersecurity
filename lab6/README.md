aa1e58991805748f87100533f39ea5487fff087a
GlennChia <glennchia7@gmail.com>
2019-10-25 00:09:39 +0800

    question 1 completed

--- lab6/README.md
+++ lab6/README.md
@@ -1,3 +1,56 @@
+# 1. Part I: Implementing Square and Multiply
+
+## 1.1 Square and multiply pseudo code is given in the notes
+
+- Convert the integer to binary and convert that to a string so we can strip the `0b` at the front
+
+## 1.2 miller_rabin
+
+Pseudo code can be found on [https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test](https://en.wikipedia.org/wiki/Miller–Rabin_primality_test) 
+
+Generating a random number in a range 
+
+-  https://docs.python.org/2/library/random.html 
+- `random.randint(a, b)` - Return a random integer *N* such that `a <= N <= b`.
+
+Illustrating a prime number example 61 with 2 rounds
+
+1. First we need to find r and d based on 61-1=60
+2. 2<sup>2</sup> * 15 = 60.
+   1. r = 2
+   2. d = 15
+3. Enter the first iteration of the loop
+   1. Pick a random integer a between 2 and 61-2=59
+   2. Let's say we choose 5
+   3. x = 5<sup>15</sup>%61 = 60
+   4. Check condition. If x=1 or x=n-1=60 we continue the loop. Since x=60 we thus continue with the loop. On to the second iteration
+4. Second iteration of the loop
+   1. This time we choose a=7
+   2. x =  7<sup>15</sup>%61 = 11
+   3. Check condition. If x=1 or x=n-1=60 we continue the loop but it is not
+   4. Now we repeat r-1 times which is just 1 time
+      1. x = 11<sup>2</sup>%61 = 60
+      2. Check if x=n-1=60
+      3. Since it is, we continue with the loop 
+5. BUT, the loops is finished already
+6. Hence the number is a prime number 
+
+## 1.3 Generating prime numbers
+
+For an n bit binary, the greatest number is 2<sup>n</sup>-1 but if there is 100 bits, this value is 1.2676505x10<sup>30</sup>
+
+With this random number generated we check if it is prime. If it is, we return it, else we generate a new number and continue the loop
+
+# 2. Part II: Diffie-Hellman Key Exchange (DHKE)
+
+
+
+# 3. Part III: Baby-Step Giant-Step Method
+
+
+
+
+
 # Open ended questions
 
 ## What’s the advantage and disadvantage of DHKE?  
--- lab6/primes_template.py
+++ lab6/primes_template.py
@@ -3,23 +3,69 @@
 
 import random
 def square_multiply(a,x,n):
-    pass
+    y = 1
+    n_b = str(bin(x)).lstrip('0b')
+    for i in range(len(n_b) - 1, -1, -1):
+        y = (y ** 2) % n
+        if n_b[i] == '1':
+            y = (a * y) % n 
+    return y
 
 def miller_rabin(n, a):
-    pass
+    # n is the number to be tested
+    # a is the number of rounds 
+    # find r and d 
+    n_sub_1 = n - 1 
+    r = 0
+    while n_sub_1 % 2 == 0:
+        n_sub_1 //= 2
+        r += 1
+    d = n_sub_1
+    # print(f'The value of d is {d}')
+    # print(f'The value of r is {r}')
+    for i in range(a):
+        a = random.randint(2, n-2) 
+        x = (a ** d) % n
+        if x == 1 or x == n-1:
+            continue
+        state = 0
+        for j in range(r-1):
+            x = (x ** 2) % n
+            if x == n-1:
+                state = 1
+                break
+        if state == 1:
+            continue
+        return 'Probably not prime'
+    return 'Probably prime'
+
 
 def gen_prime_nbits(n):
-    pass
+    # Max value of an n bit integer is 2^n - 1
+    # a = random.randint(2, 2**n -1)
+    # 100 and 80 bits takes too long. Instead we will constrain it to 20 bits and pad it to 100 and 80 bits respectively
+    a = random.randint(2, 2**20 -1)
+    # Comment the code below if we really want 100 and 80 bits
+    # a = random.randint(2, 2**n -1)
+    while miller_rabin(a, 2) == 'Probably not prime':
+        a = random.randint(2, 2**20 -1)
+    # convert to binary representration  
+    a_bin = str(bin(a)).lstrip('0b')
+    padding = n - len(a_bin)
+    pad_0 = '0' * padding
+    a_bin = pad_0 + a_bin
+    return a_bin # change this to a if we want the string representation
 
 if __name__=="__main__":
-    print 'Is 561 a prime?'
-    print miller_rabin(561,2)
-    print 'Is 27 a prime?'
-    print miller_rabin(27,2)
-    print 'Is 61 a prime?'
-    print miller_rabin(61,2)
+    print(square_multiply(2,5,5))
+    print ('Is 561 a prime?')
+    print (miller_rabin(561,2))
+    print ('Is 27 a prime?')
+    print (miller_rabin(27,2))
+    print ('Is 61 a prime?')
+    print (miller_rabin(61,2))
 
-    print 'Random number (100 bits):'
-    print gen_prime_nbits(100)
-    print 'Random number (80 bits):'
-    print gen_prime_nbits(80)
+    print ('Random number (100 bits):')
+    print (gen_prime_nbits(100))
+    print ('Random number (80 bits):')
+    print (gen_prime_nbits(80))
