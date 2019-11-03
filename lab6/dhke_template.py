94babe4426d0f5214f3b82b80a55cb6b500a2863
GlennChia <glennchia7@gmail.com>
2019-11-03 16:38:18 +0800

    template completed

--- lab6/dhke_template.py
+++ lab6/dhke_template.py
@@ -1,40 +1,55 @@
 #50.042 FCS Lab 6 template
 #Year 2019
 
-import primes 
+import primes_template 
 import random
 
 def dhke_setup(nb):
-    pass
+    # Generate an 80 bit prime number 
+    # large_prime = primes_template.gen_prime_nbits(nb)
+    # Instead of using the function, I will use Wolframalpha to save computation time
+    # https://www.wolframalpha.com/ prime closest to 2^80
+    p = 1208925819614629174706189
+    # random.seed(30)
+    # alpha = random.randint(2, p-2)
+    # alpha is a generator of the group (But we don't have a group specified in the lab handout)
+    # alpha = random.randint(2, 50) # Choose a small value to make computations fast
+    alpha = 2
+    return p, alpha
 
 def gen_priv_key(p):
-    pass
+    # random.seed(1)
+    # Comment out the one below to have a more varied private key but it will take longer to compute
+    # a = random.randint(2, p-2)
+    a = random.randint(2,10)
+    # a = 15
+    return a
 
 def get_pub_key(alpha, a, p):
-    pass
+    return primes_template.square_multiply(alpha, a, p) # (alpha ** a ) % p
 
 def get_shared_key(keypub,keypriv,p):
-    pass
+    return primes_template.square_multiply(keypub, keypriv, p) # (keypub ** keypriv) % p
     
 if __name__=="__main__":
     p,alpha= dhke_setup(80)
-    print 'Generate P and alpha:'
-    print 'P:',p
-    print 'alpha:',alpha
-    print 
+    print ('Generate P and alpha:')
+    print (f'P: {p}')
+    print (f'alpha: {alpha}')
+    print ('')
     a=gen_priv_key(p)
     b=gen_priv_key(p)
-    print 'My private key is: ',a
-    print 'Test other private key is: ',b
-    print 
+    print (f'My private key is: {a}')
+    print (f'Test other private key is: {b}')
+    print ('') 
     A=get_pub_key(alpha,a,p)
     B=get_pub_key(alpha,b,p)
-    print 'My public key is: ',A
-    print 'Test other public key is: ',B
-    print
+    print (f'My public key is: {A}')
+    print (f'Test other public key is: {B}')
+    print ('')
     sharedKeyA=get_shared_key(B,a,p)
     sharedKeyB=get_shared_key(A,b,p)
-    print 'My shared key is: ',sharedKeyA
-    print 'Test other shared key is: ',sharedKeyB
-    print 'Length of key is %d bits.'%sharedKeyA.bit_length()
+    print (f'My shared key is: {sharedKeyA}')
+    print (f'Test other shared key is: {sharedKeyB}')
+    print (f'Length of key is {sharedKeyA.bit_length()} bits.')
 
