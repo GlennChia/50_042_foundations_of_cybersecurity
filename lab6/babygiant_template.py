ceb0ad8668b3178d556b91752b46e9f2965d2e26
GlennChia <glennchia7@gmail.com>
2019-11-03 16:25:14 +0800

    Working baby giant to get shared key

--- lab6/babygiant_template.py
+++ lab6/babygiant_template.py
@@ -1,18 +1,55 @@
-#50.042 FCS Lab 6 template
-#Year 2019
+# 50.042 FCS Lab 6 template
+# Glenn Chia 1003118
+# Year 2019
 
 import math
-import primes
+import primes_template
 
 
-def baby_step(alpha,beta,p,fname):
-    pass
-    
-def giant_step(alpha,p,fname):
-    pass
+def baby_step(alpha,beta,p,fname='baby_step.txt'):
+    # Find the group order G
+    g = p - 1 # since All elements of the eld except 0 form a multiplicative group with the group operation ×
+    # Calculate m
+    m = math.floor(math.sqrt(g))
+    baby_step_result = {}
+    with open(fname, 'w') as fout:
+        for xb in range(m):
+            result = (primes_template.square_multiply(alpha, xb, p) * beta)%p
+            # result = (pow(alpha, xb, p) * beta)%p
+            fout.write(str(result) + '\n')
+            baby_step_result[xb] = result
+    return baby_step_result
+
+
+def giant_step(alpha,p,fname='giant_step.txt'):
+    g = p - 1
+    m = math.floor(math.sqrt(g))
+    giant_step_result = {}
+    with open(fname, 'w') as fout:
+        for xg in range(m):
+            result = primes_template.square_multiply(alpha, xg * m, p)
+            # result = pow(alpha, xg * m, p)# alpha ** (xg * m)
+            fout.write(str(result) + '\n')
+            giant_step_result[xg] = result
+    return giant_step_result
+
 
 def baby_giant(alpha,beta,p):
-    pass
+    g = p - 1
+    m = math.floor(math.sqrt(g))
+    baby_step_result = baby_step(alpha, beta, p)
+    giant_step_result = giant_step(alpha, p)
+    for xb, baby_value in baby_step_result.items():
+        for xg, giant_value in giant_step_result.items():
+            if baby_value == giant_value:
+                xb_final = xb
+                xg_final = xg
+    try:
+        result = xg_final * m - xb_final
+        return result
+    except:
+        return "none found"
+
 
 if __name__=="__main__":
     """
@@ -21,16 +58,26 @@ if __name__=="__main__":
     Test other private key is:  7265
     
     """
-    p=17851
-    alpha=17511
-    A=2945
-    B=11844
+    p=17851  # We modulus by this
+    alpha=17511  # This is a primitive element or generator in the group
+    A=2945  # my public key
+    B=11844  # other public key
     sharedkey=1671
     a=baby_giant(alpha,A,p)
     b=baby_giant(alpha,B,p)
-    guesskey1=primes.square_multiply(A,b,p)
-    guesskey2=primes.square_multiply(B,a,p)
-    print 'Guess key 1:',guesskey1
-    print 'Guess key 2:',guesskey2
-    print 'Actual shared key :',sharedkey
+    if b != "none found":
+        guesskey1=primes_template.square_multiply(A,b,p)
+        # guesskey1 = pow(A,b,p)
+    if a != "none found":
+        guesskey2=primes_template.square_multiply(B,a,p)
+        guesskey2 = pow(B,a,p)
+    try:
+        print (f'Guess key 1: {guesskey1}')
+    except:
+        pass
+    try:
+        print (f'Guess key 2: {guesskey2}')
+    except: 
+        pass
+    print (f'Actual shared key: {sharedkey}')
 
