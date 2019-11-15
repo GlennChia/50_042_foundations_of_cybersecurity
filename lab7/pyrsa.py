e894136374e16036177a4d4c108f2338427c01a8
GlennChia <glennchia7@gmail.com>
2019-11-15 23:08:12 +0800

    Part 1 completed

--- lab7/pyrsa.py
+++ lab7/pyrsa.py
@@ -5,16 +5,30 @@ from Crypto.Signature import PKCS1_PSS
 from Crypto.Hash import SHA256
 import argparse
 import sys
+import pyrsa_sq_mul
 
 def generate_RSA(bits=1024):
     pass
-    return private_key, public_key
+    #return private_key, public_key
     
 def encrypt_RSA(public_key_file,message):
-    pass
+    key = open(public_key_file,'r').read()
+    rsakey = RSA.importKey(key)
+    if isinstance(message, int):
+        byte_message_int = message
+    else:
+        byte_message_int = pyrsa_sq_mul.unpack_bigint(message)
+    encrypt_byte_message_int = pyrsa_sq_mul.square_multiply(byte_message_int, rsakey.e, rsakey.n) 
+    encrypt_byte_message_str = pyrsa_sq_mul.pack_bigint(encrypt_byte_message_int)
+    return encrypt_byte_message_str
 
 def decrypt_RSA(private_key_file,cipher):
-    pass
+    key = open(private_key_file,'r').read()
+    rsakey = RSA.importKey(key)
+    cipher_int = pyrsa_sq_mul.unpack_bigint(cipher)
+    decrypted_byte_message_int = pyrsa_sq_mul.square_multiply(cipher_int, rsakey.d, rsakey.n) 
+    decrypted_byte_message_str = pyrsa_sq_mul.pack_bigint(decrypted_byte_message_int)
+    return decrypted_byte_message_str
 
 def sign_RSA(private_key_loc,data):
     pass
@@ -23,4 +37,22 @@ def verify_sign(private_key_loc,sign,data):
     pass
 
 if __name__=="__main__":
-    pass
+    '''Part I: RSA Without Padding'''
+    print('Part I-------------')
+    with open("message.txt") as f_message:
+        message = f_message.read().encode()
+    print('The message to encrypt is:\n{}\n'.format(message))
+    cipher_rsa = encrypt_RSA('mykey.pem.pub', message)
+    print('The cipher text after encryption is:\n{}\n'.format(b64encode(cipher_rsa)))
+    decrypt_cipher = decrypt_RSA('mykey.pem.priv', cipher_rsa)
+    print('The original message after decryption is:\n{}\n'.format(decrypt_cipher))
+    message_hash = SHA256.new()
+    message_hash.update(message)
+    message_hash_digest = message_hash.hexdigest()
+    print('The hash of the message is:\n{}\n'.format(message_hash_digest))
+    encrypted_hash = encrypt_RSA('mykey.pem.pub', message_hash_digest.encode(encoding='utf-8'))
+    print('The signed hash is:\n{}\n'.format(b64encode(encrypted_hash)))
+    decrypt_hash = decrypt_RSA('mykey.pem.priv', encrypted_hash)
+    print('The original message after decrypting the hash:\n {}\n'.format(decrypt_hash))
+    
+
--- lab7/pyrsa_sq_mul.py
+++ lab7/pyrsa_sq_mul.py
@@ -2,8 +2,19 @@ from Crypto.PublicKey import RSA
 from base64 import b64encode,b64decode
 import random
 
+
+# Input
+# a message encode 
+# e is integer but we tread it as binary
+# n is an integer 
 def square_multiply(a,x,n):
-    pass
+    y = 1
+    n_b = str(bin(x)).lstrip('0b')
+    for i in range(len(n_b)):
+        y = (y * y) % n
+        if n_b[i] == '1':
+            y = (a * y) % n 
+    return y
 
 # function to convert long int to byte string
 def pack_bigint(i):
