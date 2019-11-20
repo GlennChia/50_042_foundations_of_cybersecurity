f42728889bca3f5220cefdbe476c4987cafa36d2
GlennChia <glennchia7@gmail.com>
2019-11-20 21:48:50 +0800

    Uncomment demo

--- lab7/part3_demo.py
+++ lab7/part3_demo.py
@@ -83,136 +83,136 @@ if __name__=="__main__":
     '''Part III: Implementing RSA with Padding - Demo RSA.'''
     HOST = '10.12.214.190'
     PORT = 8888  
-    # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    #     sock.connect((HOST, PORT))
-    #     '''
-    #     For the first part I am sending my public key and then receiving a message to be decrypted
-    #     '''
-    #     print('############')
-    #     print('FIRST PART FIRST SEGMENT: SEND PUBLIC KEY, RECEIVE MESSAGE AND CIPHER AND DECRYPT\n')
-    #     print('############')
-    #     # SEND THE PUBLIC KEY 
-    #     f_public = open('part3.pem.pub','rb')
-    #     my_public_key = f_public.read()
-    #     f_public.close()
-    #     print('My Public key sent is:\n{}\n'.format(my_public_key))
-    #     sock.send(len(my_public_key).to_bytes(4, 'big'))
-    #     print('Sent public key length: {}'.format(len(my_public_key)))
-    #     sock.sendall(my_public_key)
-    #     # RECEIVE THE MESSAGE AND DECRYPT
-    #     receive_length_segment1 = int.from_bytes(sock.recv(4), 'big')
-    #     print('Received message length: {}'.format(receive_length_segment1))
-    #     receive_message_segment1 = sock.recv(receive_length_segment1)
-    #     print('Received message:\n{}\n'.format(b64encode(receive_message_segment1)))
-    #     decrypted_message_segment1 = decrypt_RSA('part3.pem.priv', receive_message_segment1, 'part3')
-    #     print('Decrypted message is:\n{}\n'.format(decrypted_message_segment1))
-    # time.sleep(1)
-    # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    #     sock.connect((HOST, PORT))
-    #     print('############')
-    #     print('FIRST PART SECOND SEGMENT: RECEIVE PUBLIC KEY, SEND MESSAGE AND CIPHER\n')
-    #     print('############')
-    #     # get public key message length from client
-    #     pubkey_length = sock.recv(4)
-    #     pubkey_length = int.from_bytes(pubkey_length, 'big')
-    #     print("Received public key message length from client:", pubkey_length, 'bytes')
-    #     # get public key from client
-    #     pubkey = sock.recv(pubkey_length)
-    #     with open('lab7_demo.pem.pub', 'wb') as pubkeyfile:
-    #         pubkeyfile.write(pubkey)
-    #     print("Received public key:\n{}\n".format(pubkey))
-    #     # prepare message
-    #     sent_mesage_segment1 = b'Hi, Glenn.'
-    #     encrypted_message_segment1 = encrypt_RSA('lab7_demo.pem.pub', sent_mesage_segment1, 'part3')
-    #     encrypted_message_segment1_length = len(encrypted_message_segment1).to_bytes(4, 'big')
-    #     # send message length
-    #     sock.send(encrypted_message_segment1_length)
-    #     print('Sent encrypted message length: {}'.format(len(encrypted_message_segment1)))
-    #     # send message
-    #     sock.send(encrypted_message_segment1)
-    #     print("Sent encrypted message: {}".format((encrypted_message_segment1)))
-    #     print("Sent encrypted message: {}".format(b64encode(encrypted_message_segment1)))
-    #     decrypted_message_segment2 = decrypt_RSA('mykey.pem.priv', encrypted_message_segment1, 'part3')
-    #     print('Decrypted message is:\n{}\n'.format(decrypted_message_segment2))
-    # time.sleep(1)
-    # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    #     sock.connect((HOST, PORT))
-    #     '''
-    #     For the second part I am sending my public key and message that is signed with my private key
-    #     '''
-    #     print('############')
-    #     print('SECOND PART FIRST SEGMENT: SEND PUBLIC KEY, SEND MESSAGE, SEND SIGNED MESSAGE\n')
-    #     print('############')
-    #     # SEND THE PUBLIC KEY 
-    #     f_public = open('part3.pem.pub','rb')
-    #     my_public_key = f_public.read()
-    #     f_public.close()
-    #     print('My Public key sent is:\n{}\n'.format(my_public_key))
-    #     sock.send(len(my_public_key).to_bytes(4, 'big'))
-    #     print('Sent public key length: {}'.format(len(my_public_key)))
-    #     sock.sendall(my_public_key)
-    #     # SIGN THE MESSAGE
-    #     with open("mydata.txt", 'rb') as f_message_data:
-    #         message_data = f_message_data.read()
-    #     signed_hash, hashed_message = sign_RSA('part3.pem.priv', message_data)
-    #     print('The signed hash is:\n{}\n'.format(signed_hash))
-    #     # SEND THE MESSAGE LENGTH and MESSAGE
-    #     sock.send(len(message_data).to_bytes(4, 'big'))
-    #     print('Message length_sent is: {}'.format(len(message_data)))
-    #     sock.send(message_data)
-    #     print('Message sent is: {}'.format(message_data))
-    #     # SEND THE SIGNED MESSAGE
-    #     sock.send(signed_hash)
-    #     print('Raw Signed Message sent is:\n{}\n'.format(signed_hash))
-    #     print('Signed Message sent is:\n{}\n'.format(b64encode(signed_hash)))
-    #     print('JUST TO CHECK {}'.format(verify_sign('part3.pem.pub', signed_hash, message_data)))
-    #     # Receive acknowledgement from partner 
-    #     ## Receive the length of the message
-    #     receive_length = int.from_bytes(sock.recv(4), 'big')
-    #     print('Received message length: {}'.format(receive_length))
-    #     ## Receive message
-    #     receive_message = sock.recv(receive_length)
-    #     if receive_message == b'OK':
-    #         print('TRANSMISSION SUCCESS\n')
-    #     else:
-    #         print('TRANSMISSION FAILED\n')
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((HOST, PORT))
+        '''
+        For the first part I am sending my public key and then receiving a message to be decrypted
+        '''
+        print('############')
+        print('FIRST PART FIRST SEGMENT: SEND PUBLIC KEY, RECEIVE MESSAGE AND CIPHER AND DECRYPT\n')
+        print('############')
+        # SEND THE PUBLIC KEY 
+        f_public = open('part3.pem.pub','rb')
+        my_public_key = f_public.read()
+        f_public.close()
+        print('My Public key sent is:\n{}\n'.format(my_public_key))
+        sock.send(len(my_public_key).to_bytes(4, 'big'))
+        print('Sent public key length: {}'.format(len(my_public_key)))
+        sock.sendall(my_public_key)
+        # RECEIVE THE MESSAGE AND DECRYPT
+        receive_length_segment1 = int.from_bytes(sock.recv(4), 'big')
+        print('Received message length: {}'.format(receive_length_segment1))
+        receive_message_segment1 = sock.recv(receive_length_segment1)
+        print('Received message:\n{}\n'.format(b64encode(receive_message_segment1)))
+        decrypted_message_segment1 = decrypt_RSA('part3.pem.priv', receive_message_segment1, 'part3')
+        print('Decrypted message is:\n{}\n'.format(decrypted_message_segment1))
+    time.sleep(1)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((HOST, PORT))
+        print('############')
+        print('FIRST PART SECOND SEGMENT: RECEIVE PUBLIC KEY, SEND MESSAGE AND CIPHER\n')
+        print('############')
+        # get public key message length from client
+        pubkey_length = sock.recv(4)
+        pubkey_length = int.from_bytes(pubkey_length, 'big')
+        print("Received public key message length from client:", pubkey_length, 'bytes')
+        # get public key from client
+        pubkey = sock.recv(pubkey_length)
+        with open('lab7_demo.pem.pub', 'wb') as pubkeyfile:
+            pubkeyfile.write(pubkey)
+        print("Received public key:\n{}\n".format(pubkey))
+        # prepare message
+        sent_mesage_segment1 = b'Hi, Glenn.'
+        encrypted_message_segment1 = encrypt_RSA('lab7_demo.pem.pub', sent_mesage_segment1, 'part3')
+        encrypted_message_segment1_length = len(encrypted_message_segment1).to_bytes(4, 'big')
+        # send message length
+        sock.send(encrypted_message_segment1_length)
+        print('Sent encrypted message length: {}'.format(len(encrypted_message_segment1)))
+        # send message
+        sock.send(encrypted_message_segment1)
+        print("Sent encrypted message: {}".format((encrypted_message_segment1)))
+        print("Sent encrypted message: {}".format(b64encode(encrypted_message_segment1)))
+        decrypted_message_segment2 = decrypt_RSA('mykey.pem.priv', encrypted_message_segment1, 'part3')
+        print('Decrypted message is:\n{}\n'.format(decrypted_message_segment2))
+    time.sleep(1)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((HOST, PORT))
+        '''
+        For the second part I am sending my public key and message that is signed with my private key
+        '''
+        print('############')
+        print('SECOND PART FIRST SEGMENT: SEND PUBLIC KEY, SEND MESSAGE, SEND SIGNED MESSAGE\n')
+        print('############')
+        # SEND THE PUBLIC KEY 
+        f_public = open('part3.pem.pub','rb')
+        my_public_key = f_public.read()
+        f_public.close()
+        print('My Public key sent is:\n{}\n'.format(my_public_key))
+        sock.send(len(my_public_key).to_bytes(4, 'big'))
+        print('Sent public key length: {}'.format(len(my_public_key)))
+        sock.sendall(my_public_key)
+        # SIGN THE MESSAGE
+        with open("mydata.txt", 'rb') as f_message_data:
+            message_data = f_message_data.read()
+        signed_hash, hashed_message = sign_RSA('part3.pem.priv', message_data)
+        print('The signed hash is:\n{}\n'.format(signed_hash))
+        # SEND THE MESSAGE LENGTH and MESSAGE
+        sock.send(len(message_data).to_bytes(4, 'big'))
+        print('Message length_sent is: {}'.format(len(message_data)))
+        sock.send(message_data)
+        print('Message sent is: {}'.format(message_data))
+        # SEND THE SIGNED MESSAGE
+        sock.send(signed_hash)
+        print('Raw Signed Message sent is:\n{}\n'.format(signed_hash))
+        print('Signed Message sent is:\n{}\n'.format(b64encode(signed_hash)))
+        print('JUST TO CHECK {}'.format(verify_sign('part3.pem.pub', signed_hash, message_data)))
+        # Receive acknowledgement from partner 
+        ## Receive the length of the message
+        receive_length = int.from_bytes(sock.recv(4), 'big')
+        print('Received message length: {}'.format(receive_length))
+        ## Receive message
+        receive_message = sock.recv(receive_length)
+        if receive_message == b'OK':
+            print('TRANSMISSION SUCCESS\n')
+        else:
+            print('TRANSMISSION FAILED\n')
 
-    # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    #     sock.connect((HOST, PORT))
-    #     '''
-    #     For the second part I am receiving my partner's public key and message that is signed with his private key
-    #     '''
-    #     print('############')
-    #     print('SECOND PART SECOND SEGMENT: RECEIVE PUBLIC KEY, RECEIVE MESSAGE, RECEIVE SIGNED MESSAGE\n')
-    #     print('############')
-    #     # get public key message length from client
-    #     pubkey_length = sock.recv(4)
-    #     pubkey_length = int.from_bytes(pubkey_length, 'big')
-    #     print("Received public key message length from client:", pubkey_length, 'bytes')
-    #     # get public key from client
-    #     pubkey = sock.recv(pubkey_length)
-    #     with open('lab7_demo2.pem.pub', 'wb') as pubkeyfile:
-    #         pubkeyfile.write(pubkey)
-    #     print("Received public key:\n{}\n".format(pubkey))
-    #     # Receive message length and message 
-    #     message_partner_length = int.from_bytes(sock.recv(4), 'big')
-    #     print("The length of my partner's message is: {}".format(message_partner_length))
-    #     message_partner = sock.recv(message_partner_length)
-    #     print('Received message:\n{}\n'.format(message_partner))
-    #     # Receive signed message
-    #     signed_message_partner = sock.recv(128)
-    #     print('Received signed message:\n{}\n'.format(signed_message_partner))
-    #     print('Received signed message:\n{}\n'.format(b64encode(signed_message_partner)))
-    #     if verify_sign('lab7_demo2.pem.pub', signed_message_partner, message_partner) == "The signature is authentic.":
-    #         response = 'OK'
-    #         print('TRANSMISSION SUCCESS')
-    #     else:
-    #         response = 'ERR'
-    #         print('TRANSMISSION FAILED')
-    #     sock.send(len(response).to_bytes(4, 'big'))
-    #     print('Sent length of response: {}'.format(len(response).to_bytes(4, 'big')))
-    #     sock.send(bytearray(response, encoding='utf8'))
-    #     print('Sent the response')
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((HOST, PORT))
+        '''
+        For the second part I am receiving my partner's public key and message that is signed with his private key
+        '''
+        print('############')
+        print('SECOND PART SECOND SEGMENT: RECEIVE PUBLIC KEY, RECEIVE MESSAGE, RECEIVE SIGNED MESSAGE\n')
+        print('############')
+        # get public key message length from client
+        pubkey_length = sock.recv(4)
+        pubkey_length = int.from_bytes(pubkey_length, 'big')
+        print("Received public key message length from client:", pubkey_length, 'bytes')
+        # get public key from client
+        pubkey = sock.recv(pubkey_length)
+        with open('lab7_demo2.pem.pub', 'wb') as pubkeyfile:
+            pubkeyfile.write(pubkey)
+        print("Received public key:\n{}\n".format(pubkey))
+        # Receive message length and message 
+        message_partner_length = int.from_bytes(sock.recv(4), 'big')
+        print("The length of my partner's message is: {}".format(message_partner_length))
+        message_partner = sock.recv(message_partner_length)
+        print('Received message:\n{}\n'.format(message_partner))
+        # Receive signed message
+        signed_message_partner = sock.recv(128)
+        print('Received signed message:\n{}\n'.format(signed_message_partner))
+        print('Received signed message:\n{}\n'.format(b64encode(signed_message_partner)))
+        if verify_sign('lab7_demo2.pem.pub', signed_message_partner, message_partner) == "The signature is authentic.":
+            response = 'OK'
+            print('TRANSMISSION SUCCESS')
+        else:
+            response = 'ERR'
+            print('TRANSMISSION FAILED')
+        sock.send(len(response).to_bytes(4, 'big'))
+        print('Sent length of response: {}'.format(len(response).to_bytes(4, 'big')))
+        sock.send(bytearray(response, encoding='utf8'))
+        print('Sent the response')
 
     # ##########
     # # Redo the protocol attack with the new RSA.
